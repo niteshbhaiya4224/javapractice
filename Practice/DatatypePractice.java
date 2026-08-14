3252eb9160a05b7fc6bcbcdd50ba591e1ff735c6
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-14 07:47:56 +0530

    Add initial implementation of DatatypePractice class with documentation

---
+++ Practice/DatatypePractice.java
@@ -0,0 +1,13 @@
+package Practice;
+
+/*
+
+You have identify all the items that you are using in a day and you 
+try to put all that in this java practice code with their correct datatype
+
+*/
+
+public class DatatypePractice {
+    
+    
+}
