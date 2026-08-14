6690e72dca84c3339be1c97301b1ff5ac6f90283
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-14 07:40:51 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Java/DataTypes.java
@@ -0,0 +1,34 @@
+package Java;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int id = 1003;
+        long phoneNumber = 9142416282456789098L;
+
+        float pi = 3.14f;
+        double d1 = 4.789; // Highly Preferrable
+
+        boolean val = true; // It stores either true/false
+
+
+
+        System.out.println("Integer Number is : " + id);
+        System.out.println("Long Number is :" + phoneNumber);
+        System.out.println("float decimal point number is :" + pi);
+        System.out.println("double decimal point number is :" + d1);
+    }
+}
+
+/*
+
+There are two types of datatypes
+
+1. Primitive -- byte, short, int, long, float, double, boolean, character
+2. Non Primitive -- Create objects --> String, Array
+
+
+
+
+*/
