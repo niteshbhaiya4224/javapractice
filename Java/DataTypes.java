a1dd08314dacef0fcb7feb0870ae79506c32c10a
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-14 07:44:56 +0530

    Fix long phone number format and improve output readability

--- Java/DataTypes.java
+++ Java/DataTypes.java
@@ -5,19 +5,30 @@ public class DataTypes {
     public static void main(String[] args) {
 
         int id = 1003;
-        long phoneNumber = 9142416282456789098L;
+        long phoneNumber = 9142416282L;
 
         float pi = 3.14f;
         double d1 = 4.789; // Highly Preferrable
 
         boolean val = true; // It stores either true/false
 
+        char ch = '@';
 
 
+        // Non Primitive 
+
+        String str = "Edgard"; // String is a collection of character and we have to write this inside double quotes
+
+        System.out.println("\n------------------\n");
         System.out.println("Integer Number is : " + id);
         System.out.println("Long Number is :" + phoneNumber);
         System.out.println("float decimal point number is :" + pi);
         System.out.println("double decimal point number is :" + d1);
+        System.out.println("Boolean values " + val);
+        System.out.println("character is :" + ch);
+        System.out.println("String value : "+ str);
+        System.out.println("\n------------------\n");
+
     }
 }
 
