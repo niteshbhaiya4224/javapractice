58eb4cf74643bbcd56f57236a97b77318f0d435a
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-19 08:00:41 +0530

    Add initial implementation of Conditional class with main method and comments

---
+++ Java/Conditional.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class Conditional {
+    /*
+        Maria will allow john to play outside if it is not raining and John have done his homework.
+    */  
+
+        public static void main(String[] args) {
+
+            boolean willJohnPlay = false;
+            boolean isRaining = false;
+
+            // if(){
+                
+            // }
+            // else if(){
+
+            // }
+            // else{
+
+            // }
+        }
+    
+}
