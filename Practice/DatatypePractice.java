97f7f7a7f25db12bb6e1435648b74efee1068b8a
efarfan-dev <efarfan.dev@gmail.com>
2026-08-18 21:48:00 -0400

    Add additional items to DatatypePractice class

--- Practice/DatatypePractice.java
+++ Practice/DatatypePractice.java
@@ -9,5 +9,14 @@ try to put all that in this java practice code with their correct datatype
 
 public class DatatypePractice {
     
-    
+    String item1="laptop";
+    String item2 ="phone" ;
+    String item3 ="water";
+    String item4= "keys";
+    String item5="car";
+    String item6="conditioner";
+    String item7="tv";
+    String item8="food";
+    String item9="fork";
+    String item10="coffe";
 }
