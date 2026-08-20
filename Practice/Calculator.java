29de8d5d79da3b4141d136e9afb8d0b7b34fc1ff
efarfan-dev <efarfan.dev@gmail.com>
2026-08-19 22:03:29 -0400

    Implement basic calculator functionality with user input for operations

--- Practice/Calculator.java
+++ Practice/Calculator.java
@@ -1,5 +1,38 @@
 package Practice;
+import java.util.Scanner;
 
 public class Calculator {
     // User Will give input from the keyboard and you need to do operation like : + , -, x, /, %
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        double num1 = scan.nextDouble();
+
+        System.out.println("Enter operation (+,-,*,/,%)");
+        String op = scan.next();
+
+        System.out.println("Enter second number");
+        double num2 = scan.nextDouble();
+
+        if(op.equals("+")){
+            System.out.println(num1+num2);
+        }
+        else if(op.equals("-")){
+            System.out.println(num1-num2);
+        }
+        else if(op.equals("*")){
+            System.out.println(num1*num2);
+        } 
+        else if(op.equals("/")){
+            System.out.println(num1/num2);
+        }
+        else if(op.equals("%")){
+            System.out.println(num1%num2);
+        }
+        else{
+            System.out.println("invalid");
+        }
+    }
 }
+    
