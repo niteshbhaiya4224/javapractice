fbd24819366ee3ca4b04a0cdfb770c3219ea24fe
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-21 07:24:56 +0530

    Add Looping class with methods for number printing tasks

---
+++ Practice/Looping.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class Looping {
+    // Print number from 1 to 10
+    // Print the reverse from 10 to 1
+    // Print all the even numver from 1 to 30
+    // print all the numbers from 1 to 100 that are divisible by 5
+
+}
