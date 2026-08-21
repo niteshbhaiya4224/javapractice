882507edeaaf2497bb52afc6c0c948a20361013c
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-21 07:22:27 +0530

    Add Looping class with examples of for loop and infinite loop

---
+++ Java/Looping.java
@@ -0,0 +1,25 @@
+package Java;
+
+public class Looping {
+
+    public static void main(String[] args) {
+
+        // If I told you to print your name 1000 times 
+
+        for(int i = 1; i<=1000; i++){
+            System.out.println("Edgard -- "+i);
+        }
+
+        // To run a loop infinite times
+
+        // for(int i = 1; i>=-1; i++){
+        //     System.out.println("Edgard -- "+i);
+        // }
+
+        // Another example of Infinite loop
+
+        // for( ; ; ){
+        //     System.out.println("Hello Edgard");
+        // }
+    }
+}
