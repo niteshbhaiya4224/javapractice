b3f3e25ae8e06cd3927dfe86759e0570e46a13e2
efarfan-dev <efarfan.dev@gmail.com>
2026-08-20 22:07:06 -0400

    Fix loop implementations in Looping class for number printing tasks

--- Practice/Looping.java
+++ Practice/Looping.java
@@ -2,8 +2,23 @@ package Practice;
 
 public class Looping {
     // Print number from 1 to 10
+    for(int i=1; i<= 10 ; i++){
+        System.out.println(i);
+    }
     // Print the reverse from 10 to 1
+    for(int a=10; a>= 1 ; a--){
+        System.out.println(a);
+    }
     // Print all the even numver from 1 to 30
+
+    for(int b = 2; i<=30 ; i+=2){
+        System.out.println(b);
+    }
     // print all the numbers from 1 to 100 that are divisible by 5
 
+    for(int t = 1;i <= 100; i++){
+        if(t % 5 == 0){
+        System.out.println(t);
+        }
+    }
 }
