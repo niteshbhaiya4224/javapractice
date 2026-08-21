e6d6ca1bb7821ca6b0d909d87985b9783d7d8aa4
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-08-21 07:47:42 +0530

    Fix formatting and indentation in Looping class main method

--- Practice/Looping.java
+++ Practice/Looping.java
@@ -1,26 +1,30 @@
 package Practice;
 
 public class Looping {
-    public static void main(String[]args){
-    // Print number from 1 to 10
-    for(int i=1; i<= 10 ; i++){
-        System.out.println(i);
-    }
-    // Print the reverse from 10 to 1
-    for(int a=10; a>= 1 ; a--){
-        System.out.println(a);
-    }
-    // Print all the even numver from 1 to 30
-
-    for(int b = 2; b<=30 ; b+=2){
-        System.out.println(b);
-    }
-    // print all the numbers from 1 to 100 that are divisible by 5
-
-    for(int t = 1;t <= 100; t++){
-        if(t % 5 == 0){
-        System.out.println(t);
+    public static void main(String[] args) {
+        // Print number from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
+        // Print the reverse from 10 to 1
+        for (int a = 10; a >= 1; a--) {
+            System.out.println(a);
+        }
+        // Print all the even numver from 1 to 30
+
+        for (int b = 2; b <= 30; b += 2) {
+            System.out.println(b);
+        }
+        // print all the numbers from 1 to 100 that are divisible by 5
+
+        for (int t = 1; t <= 100; t++) {
+            if (t % 5 == 0) {
+                System.out.println(t);
+            }
+        }
+
+
+        // You have to take a number - 541237 you have to print the total number of digit i.e 6
+        
     }
-}
 }
