e191bc381fa73b08b29f4997d5f13a3fa4bcc97e
efarfan-dev <efarfan.dev@gmail.com>
2026-09-06 23:17:53 -0400

    Implement factorial calculation in Factorial class

--- Practice/Factorial.java
+++ Practice/Factorial.java
@@ -1,12 +1,26 @@
 package Practice;
-
+import java.util.Scanner;
 public class Factorial {
     // 5! = 5 * 4 * 3 * 2 * 1 = 120
     // 7! = 7 * 6 * 5! = 5040
     
     public static void main(String[] args) {
-        
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter number");
+        int num = scan.nextInt();
+
+        long factorial = 1;
+
+        if(num < 0){
+            System.out.println("no negative number");
+        }else{
+            for(int i = 1; i <= num; i++){
+                factorial = factorial * i;
+            }
+            System.out.println("factorial: "+ factorial);
+        }
+
 
     }
 }
