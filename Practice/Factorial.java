607516546ef79364037aec92bdd7a4c51312be68
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-09-07 08:57:28 +0530

    Refactor factorial calculation to use BigInteger for overflow handling

--- Practice/Factorial.java
+++ Practice/Factorial.java
@@ -1,4 +1,5 @@
 package Practice;
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Factorial {
     // 5! = 5 * 4 * 3 * 2 * 1 = 120
@@ -10,17 +11,26 @@ public class Factorial {
         System.out.println("enter number");
         int num = scan.nextInt();
 
-        long factorial = 1;
+        // long factorial = 1;
 
-        if(num < 0){
-            System.out.println("no negative number");
-        }else{
-            for(int i = 1; i <= num; i++){
-                factorial = factorial * i;
-            }
-            System.out.println("factorial: "+ factorial);
+        // if(num < 0){
+        //     System.out.println("no negative number");
+        // }else{
+        //     for(int i = 1; i <= num; i++){
+        //         factorial = factorial * i;
+        //     }
+        //     System.out.println("factorial: "+ factorial);
+        // }
+
+
+        // To maintain the overflow we have to use BigInteger
+
+        BigInteger factorial = new BigInteger("1");
+
+        for(int i = 1; i<=num; i++){
+            factorial = factorial.multiply(BigInteger.valueOf(i));
         }
 
-
+        System.out.println(factorial);
     }
 }
