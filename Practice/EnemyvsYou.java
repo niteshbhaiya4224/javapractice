86fbfbedc3898480a287c5278930f1fd1c3f12fa
efarfan-dev <efarfan.dev@gmail.com>
2026-09-07 23:21:20 -0400

    Enhance game logic in EnemyvsYou class with complete turn mechanics and health updates

--- Practice/EnemyvsYou.java
+++ Practice/EnemyvsYou.java
@@ -17,7 +17,46 @@ package Practice;
         If anytime anyone health become 0 that person will loose the game and opponent will win 
         
         */
-
+import java.util.Random;
 public class EnemyvsYou {
+        public static void main(String[] args) {
+                Random random = new Random();
+                int yourHealth = 100;
+                int enemyHealth = 100;
+                int perHit = 10;
+                
+                while(yourHealth > 0 && enemyHealth > 0){
+                        int yourHits = random.nextInt(4);
+                        int yourDamage = yourHits * perHit;
+                        enemyHealth = enemyHealth - yourDamage;
+
+                        if(enemyHealth <0){
+                                enemyHealth = 0;
+                        }
+                        System.out.println("Hits: "+ yourHits);
+                        System.out.println("Enemy Health: "+ enemyHealth);
+
+                        if(enemyHealth == 0){
+                                System.out.println("you win");
+                                break;
+                        }
+
+                        int enemyHits = random.nextInt(4);
+                        int enemyDamage = enemyHits * perHit;
+                        yourHealth = yourHealth - enemyDamage;
+
+                        if(yourHealth < 0){
+                                yourHealth=0;
+                        }
+                        System.out.println("Enemy hit you: "+ enemyHits);
+                        System.out.println(yourHealth);
+
+                        if(yourHealth == 0){
+                                System.out.println("Enemy wins");
+                        }
+
+                }
+                
+        }
     
 }
