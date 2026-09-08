4736baa25868105fb81349747f91ec7e8c77fc5f
niteshbhaiya4224 <nitesh616.hitit2020@gmail.com>
2026-09-08 08:35:31 +0530

    Add EnemyvsYou class with game logic and health mechanics

---
+++ Practice/EnemyvsYou.java
@@ -0,0 +1,23 @@
+package Practice;
+/*
+
+        Enemy player ----VS-----  You
+
+        Your health = 100;
+        enemy health = 100;
+
+        Per hit = -10;
+
+        you will generate a random number of bound(1,3) - (1,2,3);
+
+        whatever random number you got that means you have hit the enemy that much time - 
+
+        Now next turn will be of enemy - and whatever random number they get that much multiplied by perHit will get reduced from you 
+
+        If anytime anyone health become 0 that person will loose the game and opponent will win 
+        
+        */
+
+public class EnemyvsYou {
+    
+}
